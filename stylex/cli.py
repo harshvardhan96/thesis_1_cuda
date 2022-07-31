f263243d572e19b38f0791d1eb2439ead0c19d3f
Harsh <harshvardhanchaturvedi96@gmail.com>
2022-07-31 22:43:16 +0100

    batch size = 16 and gradient =4 and lr = default  in cli.py

--- stylex/cli.py
+++ stylex/cli.py
@@ -100,10 +100,10 @@ def train_from_folder(
         network_capacity=16,  # 16
         fmap_max=512,
         transparent=False,
-        batch_size=64,
-        gradient_accumulate_every=1,
+        batch_size=16,
+        gradient_accumulate_every=4,
         num_train_steps=150000,
-        learning_rate=0.01,
+        learning_rate=2e-4,
         lr_mlp=0.1,
         ttur_mult=1.5,
         rel_disc_loss=False,
