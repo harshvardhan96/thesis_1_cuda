cda25d641bee0709b852eeb63de9899f7d4d39ed
Harsh <harshvardhanchaturvedi96@gmail.com>
2022-08-01 02:14:00 +0100

    changed dataset path

--- stylex/cli.py
+++ stylex/cli.py
@@ -89,7 +89,7 @@ def run_training(rank, world_size, model_args, data, load_from, new, num_train_s
 
 def train_from_folder(
         # data='./data/Kaggle_FFHQ_Resized_256px/flickrfaceshq-dataset-nvidia-resized-256px/resized/',
-        data='../data/Kaggle_FFHQ_Resized_256px/flickrfaceshq-dataset-nvidia-resized-256px/resized_sub/',
+        data='../data/Kaggle_FFHQ_Resized_256px/flickrfaceshq-dataset-nvidia-resized-256px/resized/',
         results_dir='./results',
         models_dir='./models',
         name='Faces-Resnet-64',  # Name of the experiment.
